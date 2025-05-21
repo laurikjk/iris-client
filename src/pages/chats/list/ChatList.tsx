1d00dfa694ee60e79ede35517d90c03779354d1f
laurikjk <kajakko.lauri@gmail.com>
2025-05-21 13:08:42 +0300

    rm log

--- src/pages/chats/list/ChatList.tsx
+++ src/pages/chats/list/ChatList.tsx
@@ -196,8 +196,6 @@ const ChatList = ({className}: ChatListProps) => {
     return bLatest - aLatest
   })
 
-  console.log("allChats", allChats)
-
   return (
     <PublicChatContext.Provider value={{setPublicChatTimestamps}}>
       <nav className={className}>
